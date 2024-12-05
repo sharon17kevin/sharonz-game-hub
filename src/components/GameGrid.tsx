48e9ab386bbdada76cacb33fefe5f59b5f9b8296
sharon17kevin <sharon17kevin@gmail.com>
2024-12-05 20:00:39 +0100

    Refactor: Remove duplicated Card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,18 +2,30 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import SkeletonGameCard from "./SkeletonGameCard";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { gameList, error, isLoading } = useGames();
-  const skeletons = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  const skeletons = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={10} padding='10px'>
-        {isLoading && skeletons.map(skeleton=> <SkeletonGameCard key={skeleton}/>)}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={10}
+        padding="10px"
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <SkeletonGameCard key={skeleton} />
+            </GameCardContainer>
+          ))}
         {gameList.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
