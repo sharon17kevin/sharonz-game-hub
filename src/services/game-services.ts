e71f60d683e09663cc376c87edb3c7b15c804ed6
sharon17kevin <sharon17kevin@gmail.com>
2024-12-27 20:11:14 +0100

    Build Game Attributes

--- src/services/game-services.ts
+++ src/services/game-services.ts
@@ -1,15 +1,19 @@
+import { Publisher } from "../entities/Publishers";
 import APIClient from "./api-clent";
+import { Genre } from "./genre-services";
 import { Platform } from "./platform-services";
 
 export interface Game {
     id: number;
     name: string;
+    genres: Genre[];
     background_image: string;
+    publishers: Publisher[];
     parent_platforms: {platform: Platform}[];
-    metacritic: number,
-    rating_top: number,
-    slug: string,
-    description_raw: string,
+    metacritic: number;
+    rating_top: number;
+    slug: string;
+    description_raw: string;
 
 }
 
