1794e036266648b84203f049f0cecaeba7931009
sharon17kevin <sharon17kevin@gmail.com>
2025-04-18 23:55:02 +0100

    Refactoring

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,20 +5,24 @@ import usePlatfroms from "../hooks/usePlatform";
 import useGameQueryStore from "../zustand/gameQueryStore";
 
 const PlatformSelector = () => {
-    const {data, error} = usePlatfroms();
-    const selectedPlatformId = useGameQueryStore(s=>s.gameQuery.platformId)
-    const setPlatform = useGameQueryStore(s=>s.setPlatform)
-    const selectedPlatform = useGetPlatform(selectedPlatformId)
+  const { data, error } = usePlatfroms();
+  const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
+  const setPlatform = useGameQueryStore((s) => s.setPlatform);
+  const selectedPlatform = useGetPlatform(selectedPlatformId);
 
-    if (error) return;
+  if (error) return;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms'}
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data?.results.map(platform => <MenuItem onClick={()=>setPlatform(platform.id)} key={platform.id}>{platform.name}</MenuItem>)}
+        {data?.results.map((platform) => (
+          <MenuItem onClick={() => setPlatform(platform.id)} key={platform.id}>
+            {platform.name}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
